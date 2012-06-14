441e405e3707386ac5f06ece8b844c45d7c954ab
Dan Lowe <dan@tangledhelix.com>
2012-06-14 11:13:24 -0400

    retab

--- install.sh
+++ install.sh
@@ -3,24 +3,24 @@
 # For unenlightened systems where ruby and rake are not available.
 
 for item in bash bash_profile bashrc emacs gitignore inputrc \
-    ircrc oh-my-zsh screenrc tcshrc terminfo tmux.conf vim vimrc \
-    zlogin zlogout zshenv zshrc
+  ircrc oh-my-zsh screenrc tcshrc terminfo tmux.conf vim vimrc \
+  zlogin zlogout zshenv zshrc
 do
-    if [ -r $HOME/.$item ]; then
-        echo "*** WARNING: $HOME/.$item already exists"
-    else
-        echo "* .$item => .dotfiles/$item"
-        ln -s $HOME/.dotfiles/$item $HOME/.$item
-    fi
+  if [ -r $HOME/.$item ]; then
+    echo "*** WARNING: $HOME/.$item already exists"
+  else
+    echo "* .$item => .dotfiles/$item"
+    ln -s $HOME/.dotfiles/$item $HOME/.$item
+  fi
 done
 
 if [ -r $HOME/.gitconfig ]; then
-    echo "*** WARNING: $HOME/.gitconfig already exists"
+  echo "*** WARNING: $HOME/.gitconfig already exists"
 else
-    echo
-    echo "*** NOTE: set up $HOME/.gitconfig"
-    echo
-    cp $HOME/.dotfiles/gitconfig.erb $HOME/.gitconfig
-    chmod 0600 $HOME/.gitconfig
+  echo
+  echo "*** NOTE: set up $HOME/.gitconfig"
+  echo
+  cp $HOME/.dotfiles/gitconfig.erb $HOME/.gitconfig
+  chmod 0600 $HOME/.gitconfig
 fi
 
