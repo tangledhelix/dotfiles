3bd5c7a42e3ed776106b8b0c5228ea4db2a5ab86
Dan Lowe <dan@tangledhelix.com>
2013-03-18 10:30:47 -0400

    add path that actually works

--- install.pl
+++ install.pl
@@ -65,7 +65,7 @@ chdir $basedir;
 print_help() unless defined($ARGV[0]);
 my $action = $ARGV[0];
 
-$ENV{PATH} = qw( /usr/local/bin /usr/bin /bin );
+$ENV{PATH} = '/usr/local/bin:/usr/bin:/bin';
 
 if ($action eq 'bash') {
     foreach my $file (@{$files{bash}}) {
