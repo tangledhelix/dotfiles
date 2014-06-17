0756cf98d717b6a1b5bc40efa8487def2299814c
Dan Lowe <dan@tangledhelix.com>
2014-06-17 08:42:10 -0700

    Add .colordiffrc

--- install.pl
+++ install.pl
@@ -13,7 +13,7 @@ my %files = (
     bash => [ 'bash', 'bash_profile', 'bashrc' ],
     git  => [ 'gitconfig', 'gitignore' ],
     misc => [ 'cvsrc', 'emacs', 'hgrc', 'ircrc', 'pryrc', 'screenrc', 'tcshrc',
-              'terminfo', 'tmux.conf', 'perltidyrc', 'inputrc', 'psqlrc' ],
+              'terminfo', 'tmux.conf', 'perltidyrc', 'inputrc', 'psqlrc', 'colordiffrc' ],
     vim  => [ 'vim', 'vimrc' ],
     zsh  => [ 'zlogin', 'zlogout', 'zshenv', 'zshrc' ],
 );
