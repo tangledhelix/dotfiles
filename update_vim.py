8d3d122ec40c912b3e55a64488b5e3efc586d5e7
Dan Lowe <dan@tangledhelix.com>
2023-01-21 23:32:17 -0500

    New puppet plugin. Also add comments about plugins

--- update_vim.py
+++ update_vim.py
@@ -2,16 +2,27 @@
 
 # Assumes Github repos. Use shorthand "user/repo-name".
 vim_bundles = {
+    # insert and delete parens, braces, etc in pairs
     "autopairs": "jiangmiao/auto-pairs",
+    # automatically adds ending keyword to blocks (end, endif, ...)
     "endwise": "tpope/vim-endwise",
+    # github-flavored markdown plugin
     "ghmarkdown": "jtratner/vim-flavored-markdown",
+    # PaperColor theme, based on Material
     "papercolor": "NLKNguyen/papercolor-theme",
+    # Vim plugin manager
     "pathogen": "tpope/vim-pathogen",
+    # support for Perl
     "perl": "vim-perl/vim-perl",
+    # postgres-flavored SQL support
     "pgsql": "exu/pgsql.vim",
-    "puppet": "puppetlabs/puppet-syntax-vim",
+    # puppet configuration management server
+    "puppet": "rodjek/vim-puppet",
+    # status line
     "statline": "millermedeiros/vim-statline",
+    # change surrounding things (quotes, parens, etc)
     "surround": "tpope/vim-surround",
+    # easily comment / uncomment lines, blocks, selections...
     "tcomment": "tomtom/tcomment_vim"
 }
 
