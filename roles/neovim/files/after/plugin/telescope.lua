688bb04b76adb761121df192abb02139f3cfde32
Dan Lowe <dan@tangledhelix.com>
2023-02-06 00:08:08 -0500

    remap telescope binds to avoid conflict with lazygit

--- roles/neovim/files/after/plugin/telescope.lua
+++ roles/neovim/files/after/plugin/telescope.lua
@@ -2,9 +2,9 @@
 -- only use this on mac because it needs ripgrep
 if vim.fn.has('mac') == 1 then
     local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>g', builtin.git_files, {})
-    vim.keymap.set('n', '<leader>/', function()
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+    vim.keymap.set('n', '<leader>fs', function()
         builtin.grep_string({ search = vim.fn.input("Grep > ") });
     end)
 end
