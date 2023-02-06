a2eab94330cb34f29518fda4334f64d869c89326
Dan Lowe <dan.lowe@global.ntt>
2023-02-06 17:23:53 -0500

    Add lsp-zero to neovim

--- roles/neovim/files/after/plugin/lsp.lua
+++ roles/neovim/files/after/plugin/lsp.lua
@@ -1,9 +1,50 @@
 -- Learn the keybindings, see :help lsp-zero-keybindings
 -- Learn to configure LSP servers, see :help lsp-zero-api-showcase
---local lsp = require('lsp-zero')
---lsp.preset('recommended')
+local lsp = require('lsp-zero')
+lsp.preset('recommended')
+
+lsp.ensure_installed({
+    'eslint',
+    'sumneko_lua',
+})
 
 -- (Optional) Configure lua language server for neovim
---lsp.nvim_workspace()
+-- lsp.nvim_workspace()
 
---lsp.setup()
+lsp.setup()
+
+-- potential stuff to ensure_installed
+-- these are the names in the mason UI; might differ
+-- from name in this file
+--
+-- ◍ ansible-language-server
+-- ◍ beautysh
+-- ◍ black
+-- ◍ clangd
+-- ◍ css-lsp
+-- ◍ debugpy
+-- ◍ delve
+-- ◍ djlint
+-- ◍ dockerfile-language-server
+-- ◍ eslint-lsp                 ** already installed above? (eslint)
+-- ◍ firefox-debug-adapter
+-- ◍ flake8
+-- ◍ gopls
+-- ◍ hadolint
+-- ◍ html-lsp
+-- ◍ jedi-language-server
+-- ◍ json-lsp
+-- ◍ jsonlint
+-- ◍ lua-language-server       ** already installed above? (sumneko_lua)
+-- ◍ markdownlint
+-- ◍ marksman
+-- ◍ perlnavigator
+-- ◍ prettier
+-- ◍ puppet-editor-services
+-- ◍ selene
+-- ◍ sql-formatter
+-- ◍ sqlfluff
+-- ◍ sqlls
+-- ◍ stylua
+-- ◍ yaml-language-server
+-- ◍ yamllint
--- roles/neovim/files/lua/plugins.lua
+++ roles/neovim/files/lua/plugins.lua
@@ -133,31 +133,31 @@ if vim.fn.has('nvim-0.5') == 1 then
 
         use 'kdheepak/lazygit.nvim'
 
-        -- if vim.fn.has('nvim-0.7') == 1 then
+        if vim.fn.has('nvim-0.7') == 1 then
             -- https://github.com/VonHeikemen/lsp-zero.nvim
-        --     use {
-        --         'VonHeikemen/lsp-zero.nvim',
-        --         requires = {
-        --             -- LSP Support
-        --             {'neovim/nvim-lspconfig'},
-        --             {'williamboman/mason.nvim'},
-        --             {'williamboman/mason-lspconfig.nvim'},
+            use {
+                'VonHeikemen/lsp-zero.nvim',
+                branch = 'v1.x',
+                requires = {
+                    -- LSP Support
+                    {'neovim/nvim-lspconfig'},             -- Required
+                    {'williamboman/mason.nvim'},           -- Optional
+                    {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-        --             -- Autocompletion
-        --             {'hrsh7th/nvim-cmp'},
-        --             {'hrsh7th/cmp-buffer'},
-        --             {'hrsh7th/cmp-path'},
-        --             {'saadparwaiz1/cmp_luasnip'},
-        --             {'hrsh7th/cmp-nvim-lsp'},
-        --             {'hrsh7th/cmp-nvim-lua'},
+                    -- Autocompletion
+                    {'hrsh7th/nvim-cmp'},         -- Required
+                    {'hrsh7th/cmp-nvim-lsp'},     -- Required
+                    {'hrsh7th/cmp-buffer'},       -- Optional
+                    {'hrsh7th/cmp-path'},         -- Optional
+                    {'saadparwaiz1/cmp_luasnip'}, -- Optional
+                    {'hrsh7th/cmp-nvim-lua'},     -- Optional
 
-        --             -- Snippets
-        --             {'L3MON4D3/LuaSnip'},
-        --             -- Snippet Collection (Optional)
-        --             {'rafamadriz/friendly-snippets'},
-        --         }
-        --     }
-        -- end
+                    -- Snippets
+                    {'L3MON4D3/LuaSnip'},             -- Required
+                    {'rafamadriz/friendly-snippets'}, -- Optional
+                }
+            }
+        end
 
     end)
 end
