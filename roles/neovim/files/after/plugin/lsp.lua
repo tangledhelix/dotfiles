518d7a60d540904645233f3505b0c435bded4024
Dan Lowe <dan@tangledhelix.com>
2023-02-12 14:48:23 -0500

    remove extraneous notes

--- roles/neovim/files/after/plugin/lsp.lua
+++ roles/neovim/files/after/plugin/lsp.lua
@@ -18,39 +18,3 @@ if vim.fn.has('nvim-0.7') == 1 then
 
 end
 
--- potential stuff to install
---
--- these are names in the mason UI; might differ from name you'd use
--- in ensure_installed()
---
--- ◍ ansible-language-server
--- ◍ beautysh
--- ◍ black
--- ◍ clangd
--- ◍ css-lsp
--- ◍ debugpy
--- ◍ delve
--- ◍ djlint
--- ◍ dockerfile-language-server
--- ◍ eslint-lsp                 ** already installed above? (eslint)
--- ◍ firefox-debug-adapter
--- ◍ flake8
--- ◍ gopls
--- ◍ hadolint
--- ◍ html-lsp
--- ◍ jedi-language-server
--- ◍ json-lsp
--- ◍ jsonlint
--- ◍ lua-language-server       ** already installed above? (sumneko_lua)
--- ◍ markdownlint
--- ◍ marksman
--- ◍ perlnavigator
--- ◍ prettier
--- ◍ puppet-editor-services
--- ◍ selene
--- ◍ sql-formatter
--- ◍ sqlfluff
--- ◍ sqlls
--- ◍ stylua
--- ◍ yaml-language-server
--- ◍ yamllint
