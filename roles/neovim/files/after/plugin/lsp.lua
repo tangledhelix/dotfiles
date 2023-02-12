54c9f489504e5fe1a3d248ffe8ef4ca4fbe40ae6
Dan Lowe <dan@tangledhelix.com>
2023-02-12 00:05:27 -0500

    Add some version guards to after/plugin files

--- roles/neovim/files/after/plugin/lsp.lua
+++ roles/neovim/files/after/plugin/lsp.lua
@@ -1,26 +1,30 @@
--- Learn the keybindings, see :help lsp-zero-keybindings
--- Learn to configure LSP servers, see :help lsp-zero-api-showcase
-local lsp = require('lsp-zero')
-lsp.preset('recommended')
+if vim.fn.has('nvim-0.7') == 1 then
 
--- see below for more you can install in the UI (:Mason).
--- I'm not sure what their names are to put them here, or
--- how to find out in a consistent way.
-lsp.ensure_installed({
-    'eslint',
-    'sumneko_lua',
-})
+    -- Learn the keybindings, see :help lsp-zero-keybindings
+    -- Learn to configure LSP servers, see :help lsp-zero-api-showcase
+    local lsp = require('lsp-zero')
+    lsp.preset('recommended')
 
--- (Optional) Configure lua language server for neovim
--- This is so it doesn't warn you of things like 'vim'
--- not being a valid variable
-lsp.nvim_workspace()
+    -- see below for more you can install in the UI (:Mason).
+    -- I'm not sure what their names are to put them here, or
+    -- how to find out in a consistent way.
+    lsp.ensure_installed({
+        'eslint',
+        'sumneko_lua',
+    })
 
-lsp.setup()
+    -- (Optional) Configure lua language server for neovim
+    -- This is so it doesn't warn you of things like 'vim'
+    -- not being a valid variable
+    lsp.nvim_workspace()
 
-vim.diagnostic.config({
-    virtual_text = true
-})
+    lsp.setup()
+
+    vim.diagnostic.config({
+        virtual_text = true
+    })
+
+end
 
 -- potential stuff to install
 --
--- roles/neovim/files/after/plugin/undotree.lua
+++ roles/neovim/files/after/plugin/undotree.lua
@@ -1,7 +1,11 @@
-require('undotree').setup({
-    window = {
-        winblend = 12,
-    },
-})
+if vim.fn.has('nvim-0.7') == 1 then
 
-vim.keymap.set('n', '<leader>u', require('undotree').toggle, { noremap = true, silent = true })
+    require('undotree').setup({
+        window = {
+            winblend = 12,
+        },
+    })
+
+    vim.keymap.set('n', '<leader>u', require('undotree').toggle, { noremap = true, silent = true })
+
+end
