5210c1b7b0701d1e41e03b92b2ef73f6b891e5d0
Dan Lowe <dan@tangledhelix.com>
2023-02-13 02:02:38 -0500

    Don't set up nerdtree on remote servers

--- update_vim.py
+++ update_vim.py
@@ -16,8 +16,6 @@ vim_bundles = {
     "endwise": "tpope/vim-endwise",
     # github-flavored markdown plugin
     "ghmarkdown": "jtratner/vim-flavored-markdown",
-    # NERDtree file navigator
-    "nerdtree": "preservim/nerdtree",
     # PaperColor theme, based on Material
     "papercolor": "NLKNguyen/papercolor-theme",
     # Vim plugin manager
