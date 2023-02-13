2891962a9a6a894af6b46a327051066226e331b9
Dan Lowe <dan@tangledhelix.com>
2023-02-13 02:04:55 -0500

    autocmd - briefly flash what was yanked, on each yank.

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -3,3 +3,20 @@ require("set")
 require("remap")
 require("filetype")
 
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+-- when yanking text, briefly flash a selection visually to show what was
+-- yanked (40ms)
+local yank_group = augroup('HighlightYank', {})
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
