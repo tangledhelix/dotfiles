253c3a0001ab8cc0bad2a29b6660e3d5441e0d9a
Dan Lowe <dan@tangledhelix.com>
2023-02-12 21:22:32 -0500

    Update the preset for lsp-zero
    In particular, don't ask to install LSPs. I'll do
    that by hand if I want to.

--- roles/neovim/files/after/plugin/lsp.lua
+++ roles/neovim/files/after/plugin/lsp.lua
@@ -2,8 +2,12 @@ if vim.fn.has('nvim-0.7') == 1 then
 
     -- Learn the keybindings, see :help lsp-zero-keybindings
     -- Learn to configure LSP servers, see :help lsp-zero-api-showcase
-    local lsp = require('lsp-zero')
-    lsp.preset('recommended')
+    local lsp = require('lsp-zero').preset({
+        name = 'minimal',
+        set_lsp_keymaps = true,
+        manage_nvim_cmp = true,
+        suggest_lsp_servers = false,
+    })
 
     -- (Optional) Configure lua language server for neovim
     -- This is so it doesn't warn you of things like 'vim'
