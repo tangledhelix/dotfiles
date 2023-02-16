b2479ab17ec4be9d2d9a3bcf30328928192244a8
Dan Lowe <dan@tangledhelix.com>
2023-02-16 00:57:47 -0500

    indent of 2 for js, json, yaml

--- roles/neovim/files/lua/filetype.lua
+++ roles/neovim/files/lua/filetype.lua
@@ -1,8 +1,32 @@
 -- https://neovim.io/doc/user/lua.html#vim.filetype.add()
 
+local function indent_by_two()
+    vim.opt.tabstop = 2
+    vim.opt.softtabstop = 2
+    vim.opt.shiftwidth = 2
+end
+
 vim.filetype.add({
     pattern = {
         ['.*%.lua%.j2'] = 'lua',
+    },
+    extension = {
+        js = function()
+            indent_by_two()
+            return 'javascript'
+        end,
+        yaml = function()
+            indent_by_two()
+            return 'yaml'
+        end,
+        yml = function()
+            indent_by_two()
+            return 'yaml'
+        end,
+        json = function()
+            indent_by_two()
+            return 'json'
+        end,
     },
 })
 
