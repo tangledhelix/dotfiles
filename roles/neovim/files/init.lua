8ea385952445374194f5bcde83ef3f2c4e53543a
Dan Lowe <dan.lowe@global.ntt>
2026-05-08 12:01:13 -0400

    refactor (file organization, MOSTLY...)
    - adds <leader>n keymap to toggle line numbering
    - drop <leader>w remap to ^W
    - enables "delete space at EOL" except in Markdown

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -34,21 +34,82 @@ vim.pack.add({
 
 vim.g.mapleader = ' '
 
--- https://github.com/nvim-orgmode/orgmode/blob/master/docs/configuration.org
+-- line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.keymap.set('n', '<leader>n', ':set number!<CR>:set relativenumber!<CR>', { silent = true })
 
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = true
+vim.keymap.set('n', '<leader>z', ':set wrap!<CR>:set wrap?<CR>')
+-- break at word, not mid-word, if wrap is on
+vim.opt.linebreak = true
+-- visually indicate a break at left window edge
+vim.opt.showbreak = '⟩⟩'
+
+vim.opt.cursorline = true
+
+vim.opt.scrolloff = 2
+
+-- cursor styles; mostly this is defaults with blinking added
+vim.opt.guicursor = 'n-v-c-sm:block-blinkon500-blinkoff500,'
+                 .. 'i-ci-ve:ver25-blinkon500-blinkoff500,'
+                 .. 'r-cr-o:hor20-blinkon500-blinkoff500,'
+                 .. 't:block-blinkon500-blinkoff500-TermCursor'
+
+-- uncomment to disable mouse support (default is 'nvi')
+--opt.mouse = ''
+
+if vim.fn.has('mac') == 1 then
+  -- interact with system clipboard (normal or visual modes)
+  vim.keymap.set({ 'n', 'x' }, '<leader>y', '"+y')
+  vim.keymap.set({ 'n', 'x' }, '<leader>p', '"+p')
+  vim.keymap.set({ 'n', 'x' }, '<leader>P', '"+P')
+end
+
+vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
+
+-- stretch for Esc less, handy sometimes
+vim.keymap.set('i', 'kj', '<Esc>', {})
+vim.keymap.set('i', 'jj', '<Esc>', {})
+
+-- saner j,k movements
+vim.keymap.set({'n', 'x'}, 'j', 'gj', { noremap = true })
+vim.keymap.set({'n', 'x'}, 'k', 'gk', { noremap = true })
+vim.keymap.set({'n', 'x'}, 'gj', 'j', { noremap = true })
+vim.keymap.set({'n', 'x'}, 'gk', 'k', { noremap = true })
+
+-- use J/K in visual mode to move lines (and re-indent)
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- --------------------------------------------------------------------------
+-- orgmode
+
+-- https://github.com/nvim-orgmode/orgmode/blob/master/docs/configuration.org
 require('orgmode').setup({
   org_agenda_files = '~/orgfiles/**/*',
   org_default_notes_file = '~/orgfiles/refile.org',
   org_todo_keywords = { 'TODO', 'NEXT', '|', 'DONE' },
 
-  -- overview, content, showeverything, inherit
+  -- [overview, content, showeverything, inherit]
   org_startup_folded = 'content',
 
-  -- default 1 (Mon), set 0 for Sun
+  -- Default 1 (Mon), set 0 for Sun
   calendar_week_start_day = 0,
 
-  -- I set this to avoid weird highlights from '$',
-  -- not because I use LaTeX
+  -- Avoids weird highlight of '$'; I don't use LaTeX
   org_highlight_latex_and_related = 'entities',
 
   org_todo_keyword_faces = {
@@ -62,22 +123,22 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.bo.shiftwidth = 2
     vim.bo.softtabstop = 2
 
-    -- improve display of links.
+    -- Improve display of links
     vim.wo.conceallevel = 2
     vim.wo.concealcursor = 'nc'
     vim.wo.wrap = false
 
-    -- add another item to current headings or list
+    -- shift-enter: add another item to list, or add a heading
     vim.keymap.set('i', '<S-CR>', '<cmd>lua require("orgmode").action("org_mappings.meta_return")<CR>', {
       silent = true,
       buffer = true,
     })
 
-    -- indent or outdent by shiftwidth
+    -- insert mode: tab indents or outdents by shiftwidth
     vim.keymap.set('i', '<Tab>', '<Esc>>>A', { silent = true, buffer = true })
     vim.keymap.set('i', '<S-Tab>', '<Esc><<A', { silent = true, buffer = true })
 
-    -- reclaim {, } functionality taken over by orgmode
+    -- reclaim the { } functionality taken over by orgmode
     vim.keymap.set('n', 'g{', '{', { noremap = true })
     vim.keymap.set('n', 'g}', '}', { noremap = true })
   end
@@ -86,6 +147,7 @@ vim.api.nvim_create_autocmd('FileType', {
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'orgagenda',
   callback = function()
+    -- grow or shrink agenda window with >, <
     vim.keymap.set('n', '>', ':wincmd +<CR>', { noremap = true, silent = true })
     vim.keymap.set('n', '<', ':wincmd -<CR>', { noremap = true, silent = true })
   end
@@ -93,6 +155,27 @@ vim.api.nvim_create_autocmd('FileType', {
 
 -- Experimental LSP support
 vim.lsp.enable('org')
+
+-- prettification of bullets
+require('org-bullets').setup()
+
+-- start up orgmode how I like it
+vim.api.nvim_create_user_command('Orgstart', function()
+  vim.cmd('cd ~/orgfiles')
+  vim.cmd('edit main.org')
+  require('harpoon'):list():add()
+  vim.cmd('norm zMzX')
+  vim.cmd('vsplit')
+  vim.cmd('edit refile.org')
+  require('harpoon'):list():add()
+  vim.cmd('wincmd l')
+  vim.cmd('Org agenda a')
+  vim.cmd('2sleep')
+  vim.cmd('norm vd.')
+end, {})
+
+-- --------------------------------------------------------------------------
+-- Harpoon
 
 local harpoon = require('harpoon')
 harpoon:setup({
@@ -105,19 +188,31 @@ harpoon:setup({
 vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end)
 vim.keymap.set('n', '<leader><leader>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
+-- --------------------------------------------------------------------------
+-- FTerm - terminal window popover
+
 require('FTerm').setup({ border = 'double', blend = 0 })
 vim.keymap.set('n', '<C-t>', '<CMD>lua require("FTerm").toggle()<CR>')
 vim.keymap.set('t', '<C-t>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
 
--- ^G plus a register name pastes to FTerm panel (e.g. ^Ga for register a)
--- default register doesn't work with this.
+-- ^G plus a register name pastes to FTerm panel (e.g. ^Ga for register a).
+-- the default register doesn't work with this.
 vim.cmd([[tnoremap <expr> <C-G> '<C-\><C-N>"'.nr2char(getchar()).'pi']])
+
+-- --------------------------------------------------------------------------
+-- lualine - status line
 
 require('lualine').setup({
   options = { theme = 'powerline_dark' },
 })
 
+-- --------------------------------------------------------------------------
+-- lazygit
+
 vim.keymap.set('n', '<leader>g', require('lazygit').lazygit, { noremap = true, silent = true })
+
+-- --------------------------------------------------------------------------
+-- Telescope
 
 require('telescope').setup()
 
@@ -130,7 +225,7 @@ require('telescope').setup()
 --  }
 --}
 
--- some of these are off because LSPs aren't installed
+-- some of these are off because related LSPs / plugins aren't installed
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>fa', builtin.resume, { desc = 'Find Again (open picker in previous state)' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find Buffer' })
@@ -156,40 +251,13 @@ vim.keymap.set('n', '<leader>fot', orgext.search_tags, { desc = 'Org tags' })
 vim.keymap.set('n', '<leader>for', orgext.refile_heading, { desc = 'Org refile' })
 vim.keymap.set('n', '<leader>fol', orgext.insert_link, { desc = 'Org insert link' })
 
-require('org-bullets').setup()
+-- --------------------------------------------------------------------------
+-- nvim-surround plugin
 
-vim.opt.number = true
-vim.opt.relativenumber = true
+require('nvim-surround').setup()
 
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.incsearch = true
-vim.opt.hlsearch = true
-
-vim.opt.tabstop = 8
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-
-vim.opt.wrap = true
-vim.keymap.set('n', '<leader>z', ':set wrap!<CR>:set wrap?<CR>')
--- break at word, not mid-word, if wrap is on
-vim.opt.linebreak = true
--- visually indicate a break at left window edge
-vim.opt.showbreak = '⟩⟩'
-
-vim.opt.cursorline = true
-
-vim.opt.scrolloff = 2
-
-vim.opt.guicursor = 'n-v-c-sm:block-blinkon500-blinkoff500,'
-                 .. 'i-ci-ve:ver25-blinkon500-blinkoff500,'
-                 .. 'r-cr-o:hor20-blinkon500-blinkoff500,'
-                 .. 't:block-blinkon500-blinkoff500-TermCursor'
-
--- disable mouse support (default is 'nvi')
---opt.mouse = ''
+-- --------------------------------------------------------------------------
+-- autocmds
 
 -- when yanking text, briefly flash a selection visually to show what was
 -- yanked (40ms)
@@ -203,7 +271,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
--- line numbers in quickfix/location windows
+-- use line numbers in quickfix/location windows
 --vim.api.nvim_create_autocmd('Filetype', {
 --  pattern = 'qf',
 --  callback = function()
@@ -213,80 +281,33 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 --})
 
 -- delete trailing whitespace on lines, when saving.
--- Except if it's markdown, then don't. Markdown can use EOL spaces
+-- except if it's markdown, then don't. markdown can use EOL spaces
 -- for paragraph formatting, which I want sometimes.
---vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
---  pattern = '*',
---  callback = function()
---    if vim.bo.filetype ~= 'markdown' then
---      vim.cmd([[%s/\s\+$//e]])
---    end
---  end,
---})
-
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'lua',
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*',
   callback = function()
-    vim.bo.shiftwidth = 2
-    vim.bo.softtabstop = 2
-  end
+    if vim.bo.filetype ~= 'markdown' then
+      vim.cmd([[%s/\s\+$//e]])
+    end
+  end,
 })
 
-if vim.fn.has('mac') == 1 then
-  -- interact with system clipboard (normal or visual modes)
-  vim.keymap.set({ 'n', 'x' }, '<leader>y', '"+y')
-  vim.keymap.set({ 'n', 'x' }, '<leader>p', '"+p')
-  vim.keymap.set({ 'n', 'x' }, '<leader>P', '"+P')
-end
-
-vim.keymap.set('n', '<leader>e', vim.cmd.Ex)
-
--- stretch for Esc less...
-vim.keymap.set('i', 'kj', '<Esc>', {})
-vim.keymap.set('i', 'jj', '<Esc>', {})
-
--- and Ctrl...
-vim.keymap.set('n', '<leader>w', '<C-w>', {})
-
--- saner j,k movements
-vim.keymap.set({'n', 'x'}, 'j', 'gj', { noremap = true })
-vim.keymap.set({'n', 'x'}, 'k', 'gk', { noremap = true })
-vim.keymap.set({'n', 'x'}, 'gj', 'j', { noremap = true })
-vim.keymap.set({'n', 'x'}, 'gk', 'k', { noremap = true })
-
--- use J/K in visual mode to move lines (and re-indent)
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
-
-vim.api.nvim_create_user_command('Orgstart', function()
-  vim.cmd('cd ~/orgfiles')
-  vim.cmd('edit main.org')
-  require('harpoon'):list():add()
-  vim.cmd('norm zMzX')
-  vim.cmd('vsplit')
-  vim.cmd('edit refile.org')
-  require('harpoon'):list():add()
-  vim.cmd('wincmd l')
-  vim.cmd('Org agenda a')
-  vim.cmd('2sleep')
-  vim.cmd('norm vd.')
-end, {})
-
-require('nvim-surround').setup()
+-- --------------------------------------------------------------------------
+-- per-filetype settings
 
 -- https://neovim.io/doc/user/lua.html#vim.filetype.add()
-
 local function indent_by_two()
-  vim.opt.tabstop = 2
-  vim.opt.softtabstop = 2
-  vim.opt.shiftwidth = 2
+  vim.bo.tabstop = 2
+  vim.bo.softtabstop = 2
+  vim.bo.shiftwidth = 2
 end
 
 vim.filetype.add({
-  pattern = {
-    ['.*%.lua%.j2'] = 'lua',
-  },
   extension = {
+    lua = function()
+      indent_by_two()
+      return 'lua'
+    end,
     js = function()
       indent_by_two()
       return 'javascript'
