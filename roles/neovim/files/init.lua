3be0f6dab2083512d8c95c6af900c43f6b2439c0
Dan Lowe <dan.lowe@global.ntt>
2026-05-08 15:12:09 -0400

    don't mess with orgmode wrapping, for now...
    this commit contains some clanker-authored events to toggle wrap based
    on what mode you're in (insert vs. not). disabled for now.

--- roles/neovim/files/init.lua
+++ roles/neovim/files/init.lua
@@ -126,6 +126,7 @@ vim.api.nvim_create_autocmd('FileType', {
     -- Improve display of links
     vim.wo.conceallevel = 2
     vim.wo.concealcursor = 'nc'
+    --vim.wo.wrap = false
 
     -- shift-enter: add another item to list, or add a heading
     vim.keymap.set('i', '<S-CR>', '<cmd>lua require("orgmode").action("org_mappings.meta_return")<CR>', {
@@ -144,16 +145,16 @@ vim.api.nvim_create_autocmd('FileType', {
     -- Toggle wrap mode when switching into insert mode.
     -- I prefer wrap be on while editing, but off when viewing because of
     -- concealed link display (specific to orgmode and conceallevel 2).
-    vim.api.nvim_create_autocmd('InsertEnter', {
-      callback = function()
-        vim.wo.wrap = true
-      end,
-    })
-    vim.api.nvim_create_autocmd('InsertLeave', {
-      callback = function()
-        vim.wo.wrap = false
-      end,
-    })
+    --vim.api.nvim_create_autocmd('InsertEnter', {
+    --  callback = function()
+    --    vim.wo.wrap = true
+    --  end,
+    --})
+    --vim.api.nvim_create_autocmd('InsertLeave', {
+    --  callback = function()
+    --    vim.wo.wrap = false
+    --  end,
+    --})
 
   end
 })
