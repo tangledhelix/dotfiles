84a0c81928634e974877d9683da1cdb31e2b8d3d
Dan Lowe <dan.lowe@global.ntt>
2026-05-25 00:18:00 -0400

    tidy up the scripts role.

--- roles/scripts/files/collapse-all-whitespace.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-lines = STDIN.read.split("\n")
-
-for line in lines do
-  puts line.strip.gsub(/\s+/, ' ')
-end
-
--- roles/scripts/files/count.pl
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env perl
-
-for my $i (5 .. 254) {
-    printf "192.102.248.%-3d         -- UNUSED --\n", $i;
-}
