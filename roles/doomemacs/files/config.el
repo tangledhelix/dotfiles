1067cf7b144cf31d089a98a1dd369e112afba560
Dan Lowe <dan.lowe@global.ntt>
2026-06-04 15:04:16 -0400

    default line numbers off

--- roles/doomemacs/files/config.el
+++ roles/doomemacs/files/config.el
@@ -37,7 +37,7 @@
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 ;(setq display-line-numbers-type t)
-(setq display-line-numbers-type 'relative)
+(setq display-line-numbers-type nil)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
