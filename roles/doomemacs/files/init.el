438b8eb2b5b21671f780014a5aff63420e242327
Dan Lowe <dan@tangledhelix.com>
2026-08-22 14:13:01 -0400

    add php mode

--- roles/doomemacs/files/init.el
+++ roles/doomemacs/files/init.el
@@ -159,7 +159,7 @@
        ;;ocaml             ; an objective camel
        ;;odin              ; C, minus its footguns
        org               ; organize your plain life in plain text
-       ;;php               ; perl's insecure younger brother
+       php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
        ;;purescript        ; javascript, but functional
