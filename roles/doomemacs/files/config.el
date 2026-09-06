7b7b74a16c2625f2549de878b94de4af0fa3b717
Dan Lowe <dan@tangledhelix.com>
2026-09-06 00:17:08 -0400

    another theme!

--- roles/doomemacs/files/config.el
+++ roles/doomemacs/files/config.el
@@ -33,7 +33,7 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 ;;(setq doom-theme 'doom-one)
-(setq doom-theme 'doom-dark+)
+(setq doom-theme 'doom-1337)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
