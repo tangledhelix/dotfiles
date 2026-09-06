d14ad119df572db4bb7f9e5a82392d06596eb126
Dan Lowe <dan@tangledhelix.com>
2026-09-06 00:12:42 -0400

    new doom theme

--- roles/doomemacs/files/config.el
+++ roles/doomemacs/files/config.el
@@ -32,7 +32,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+;;(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-dark+)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
